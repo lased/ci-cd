2e5faf614da46286634c6939666953f94dae7b38
lased <sokollased@yandex.ru>
2024-10-07 08:30:26 +0300

    add services

---
+++ frontend/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/ping', (_, res) => {
+  return res.json({ ok: 'frontend' });
+});
+
+app.listen(PORT, () => {
+  process.stdout.write(`Server is running on port ${PORT}`);
+});
